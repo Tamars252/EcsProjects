c408fa334a8c9e6b5444fb1ecd82090ebcdbff82
Tamars252 <113263334+Tamars252@users.noreply.github.com>
2025-08-20 16:39:39 -0400

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,26 @@
+#Tamar Shuster
+
+import pandas as pd #xxlrd xlrd3 try to install if pandas isn't working
+xl = pd.read_excel("P2M012_G7.xlsx")
+# sets two global variables (bins and chars) to the values set in
+# ^the excel file
+bins = list(xl["Bin"]) #sets binary numbers to list as bins
+chars = list(xl["Char"]) #sets characters to list as chars
+
+compareDict = {} #empty dictionary: compares pairs from bins and chars lists
+
+chars2 = [] # empty list: initializing the end list
+for key in bins:
+    for string in chars:
+        if string == "\\n": #checks if the value in chars is \\n
+            string == ("\n") #sets the value in chars as \n if its \\n
+        compareDict[key]= string #sets pairs from both lists as loop iterates
+        chars2.append(string) #makes new lists without \\n
+
+        #below removes \\n from the char list
+        if string == "\n":
+            chars.remove('\\n')
+        else:
+            chars.remove(string)
+
+        break
